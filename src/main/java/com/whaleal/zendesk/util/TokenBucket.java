1a56b8cfc145b6da3663f4c5ebb11cdd78697901
whalefalls0807 <2450706719@qq.com>
2023-10-12 11:35:21 +0800

    限速代码实现（未绑定到请求中）

---
+++ src/main/java/com/whaleal/zendesk/util/TokenBucket.java
@@ -0,0 +1,44 @@
+package com.whaleal.zendesk.util;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class TokenBucket {
+    private final BlockingQueue<Object> bucket;
+    private final int capacity;
+    private final int rate;
+
+    public TokenBucket(int capacity, int rate) {
+        this.capacity = capacity;
+        this.rate = rate;
+        this.bucket = new ArrayBlockingQueue<>(capacity);
+
+        Thread tokenGenerator = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000 / rate);
+                    bucket.put(new Object());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        tokenGenerator.start();
+    }
+
+    public void acquire() {
+        try {
+            bucket.take();
+            System.out.println("Request processed");
+        } catch (InterruptedException e) {
+            System.out.println("Request rejected");
+        }
+    }
+
+    public static void main(String[] args) {
+        TokenBucket tokenBucket = new TokenBucket(10, 1); // 桶容量为10，每秒生成1个令牌
+        for (int i = 0; i < 20; i++) {
+            tokenBucket.acquire();
+        }
+    }
+}
