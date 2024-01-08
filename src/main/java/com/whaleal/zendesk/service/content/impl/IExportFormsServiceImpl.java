a6d8df7a016d9c58ad0329e71930c00f7006d7dd
QINGYUANY2 <qingyuanyang@QingyuandeMacBook-Pro.local>
2024-01-08 18:28:39 +0800

    更新ticket form 默认form更新，未测试

--- src/main/java/com/whaleal/zendesk/service/content/impl/IExportFormsServiceImpl.java
+++ src/main/java/com/whaleal/zendesk/service/content/impl/IExportFormsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.whaleal.zendesk.service.content.impl;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.whaleal.zendesk.common.ExportEnum;
 import com.whaleal.zendesk.model.ModuleRecord;
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
@@ -26,18 +28,33 @@ public class IExportFormsServiceImpl extends BaseExportService implements IExpor
         endModuleRecord(moduleRecord, useTime);
     }
 
+    //还没测试，明天来了 测试
     @Override
     public void importTicketForms() {
         ModuleRecord moduleRecord = beginModuleRecord("importTicketForms");
         long startTime = System.currentTimeMillis();
+        String TargetDefaultId = null;
         List<Document> list = mongoTemplate.find(new Query(new Criteria("domain").is(StringSub.getDomain(this.sourceDomain))), Document.class, ExportEnum.TICKET.getValue() + "_forms");
+        JSONObject request = null;
         for (Document document : list) {
             try {
                 JSONObject jsonObject = JSONObject.parseObject(document.toJson());
                 JSONObject requestParam = new JSONObject();
-                if(jsonObject.get("default"))
-                requestParam.put("ticket_form", jsonObject);
-                JSONObject request = this.doPost("/api/v2/ticket_forms", requestParam);
+                if(jsonObject.get("default").equals("true")){
+                    JSONObject getDefaultTarget = doGetTarget("/api/v2/ticket_forms", new HashMap<>());
+                    JSONArray nestedDefaultTarget = getDefaultTarget.getJSONArray("ticket_forms");
+                    for (Object nestedDefaultObj : nestedDefaultTarget) {
+                        JSONObject nestedDefault = (JSONObject) nestedDefaultObj;
+                        if(nestedDefault.get("default").equals("true")){
+                            TargetDefaultId = nestedDefault.get("id").toString();
+                        }
+                    }
+                    requestParam.put("ticket_form", jsonObject);
+                    request = this.doUpdate("/api/v2/ticket_forms", requestParam ,TargetDefaultId);
+                }else {
+                    requestParam.put("ticket_form", jsonObject);
+                    request = this.doPost("/api/v2/ticket_forms", requestParam);
+                }
                 document.put("newId", request.getJSONObject("ticket_form").get("id"));
                 log.info("importTicketForms 执行完毕,请求参数：{},执行结果{}", requestParam, request);
                 mongoTemplate.save(document, ExportEnum.TICKET.getValue() + "_forms");
